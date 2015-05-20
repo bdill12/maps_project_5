60cccafd3e2778241fca5bba3575d3e5b27a4b6e
Brenton Dill <bdill12@gmail.com>
2015-05-20 02:18:44 -0700

    Add Some Style
    Add bricks.jpg
    Style map with styleArray

--- js/main.js
+++ js/main.js
@@ -1,17 +1,49 @@
 // Initialize the Map after DOM is loaded.
 $(function initialize() {
-	'use strict';
-	var mapOptions = {
-		center: new google.maps.LatLng(34.146580, -118.147700),
-        zoom: 16,
-        mapTypeControl: true,
-        mapTypeControlOptions: {
-        	position: google.maps.ControlPosition.LEFT_BOTTOM
-        }
-    };
-    console.log('mapOptions set...');
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-    	mapOptions);
-    console.log('map object created...');
-});
+  'use strict';
+	// Set Map Options
+  var mapOptions = {
+    center: new google.maps.LatLng(34.146580, -118.147700),
+    zoom: 16,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.LEFT_BOTTOM
+    }
+  };
 
+	// Create Map Object
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+	//Style the Map's color, and labels with styleArray
+  var styleArray = [{
+    featureType: "all",
+    stylers: [{
+      saturation: -80
+    }]
+  }, {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [{
+      hue: "#00ffee"
+    }, {
+      saturation: 50
+    }]
+  }, {
+    featureType: "road",
+    elementType: "labels",
+    stylers: [{
+      visibility: "off"
+    }]
+  }, {
+    featureType: "poi.business",
+    elementType: "labels",
+    stylers: [{
+      visibility: "off"
+    }]
+  }];
+	
+	// Implement Style
+  map.setOptions({styles: styleArray});
+
+});
