cfa2784fa060a63d42cc0c396b2501ba2594360f
Brenton Dill <bdill12@gmail.com>
2015-05-20 07:54:48 -0700

    Custom Icons, Knockout
    Added Icons and knockout now controls the markers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+var markersArray = [];
 // Initialize the Map after DOM is loaded.
 $(function initialize() {
   'use strict';
+  var oldTownPasadena = new google.maps.LatLng(34.146580, -118.147700);
   // Set Map Options
   var mapOptions = {
-    center: new google.maps.LatLng(34.146580, -118.147700),
+    center: oldTownPasadena,
     zoom: 16,
     mapTypeControl: true,
     mapTypeControlOptions: {
@@ -50,51 +52,74 @@ $(function initialize() {
 
   //Create Markers
   var makeMyMarkers = function() {
-    var markersArray = [{
+    markersArray = [{
       name: "King Taco",
-      location: new google.maps.LatLng(34.146699, -118.147875)
+      location: new google.maps.LatLng(34.146699, -118.147875),
+      icon: "js/icons/restaurant_mexican.png"
     }, {
       name: "Penny's Cheesecake Factory from The Big Bang Theory",
-      location: new google.maps.LatLng(34.145522, -118.150697)
+      location: new google.maps.LatLng(34.145522, -118.150697),
+      icon: "js/icons/tv.png" 
     }, {
       name: "Barney's Beanery",
-      location: new google.maps.LatLng(34.146100, -118.148334)
+      location: new google.maps.LatLng(34.146100, -118.148334),
+      icon: "js/icons/bar.png"
     }, {
       name: "Crepe Studio",
-      location: new google.maps.LatLng(34.146976, -118.148322)
+      location: new google.maps.LatLng(34.146976, -118.148322),
+      icon: "js/icons/icecream.png"
     }, {
-      name: "Pop! Champagne and Dessert Bar",
-      location: new google.maps.LatLng(34.147078, -118.149716)
+      name: "Pop! Champagne Bar",
+      location: new google.maps.LatLng(34.147078, -118.149716),
+      icon: "js/icons/winebar.png"
     }, {
       name: "Pasadena Antique Mall",
-      location: new google.maps.LatLng(34.144846, -118.143920)
+      location: new google.maps.LatLng(34.144846, -118.143920),
+      icon: "js/icons/artgallery.png"
     }, {
       name: "Paseo Colorado",
-      location: new google.maps.LatLng(34.145246, -118.144314)
+      location: new google.maps.LatLng(34.145246, -118.144314),
+      icon: "js/icons/mall.png"
     }, {
       name: "Colorado Boulevard",
-      location: new google.maps.LatLng(34.145522, -118.150697)
+      location: new google.maps.LatLng(34.145700, -118.15000),
+      icon: "js/icons/departmentstore.png"
     }, {
-      name: "Pasadena City Hall, AKA Pawnee City Hall",
-      location: new google.maps.LatLng(34.147633, -118.143566)
+      name: "Pasadena City Hall",
+      location: new google.maps.LatLng(34.147633, -118.143566),
+      icon: "js/icons/tv.png"
     }, {
       name: "Memorial Park",
-      location: new google.maps.LatLng(34.148336, -118.147454)
+      location: new google.maps.LatLng(34.148336, -118.147454),
+      icon: "js/icons/flowers.png"
     }, {
       name: "Pasadena Civic Auditorium",
-      location: new google.maps.LatLng(34.143771, -118.144274)
+      location: new google.maps.LatLng(34.143771, -118.144274),
+      icon: "js/icons/ticket_office2.png"
     }, {
       name: "iPic Theaters",
-      location: new google.maps.LatLng(34.146569, -118.151480)
+      location: new google.maps.LatLng(34.146569, -118.151480),
+      icon: "js/icons/movierental.png"
     }];
     for (var i = 0; i < markersArray.length; i++) {
-      var marker = new google.maps.Marker({
-        position: markersArray[i].location,
-        map: map,
-        title: markersArray[i].name
-      });
-    }
-  }
+        var image = markersArray[i].icon;
+				var marker = new google.maps.Marker({
+					position: markersArray[i].location,
+					map: map,
+					title: markersArray[i].name,
+          icon: image
+				});
+      }
+};
 	makeMyMarkers();
+
+var ViewModel = {
+  myMarkers: ko.observableArray()
+};
+for (var i = 0; i < markersArray; i++) {
+  ViewModel.myMarkers.push(markersArray[i]);
+}
+
+ko.applyBindings(window.ViewModel);
 
 });
