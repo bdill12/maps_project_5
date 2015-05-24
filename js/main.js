144f18a993ef2fef465dd16a0b9c069d25a3861c
Brenton Dill <bdill12@gmail.com>
2015-05-24 08:53:11 -0700

    hu

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
-var markersArray = [];
 // Initialize the Map after DOM is loaded.
 $(function initialize() {
   'use strict';
   var oldTownPasadena = new google.maps.LatLng(34.146580, -118.147700);
-  // Set Map Options
+  //  Set Map Options
   var mapOptions = {
     center: oldTownPasadena,
     zoom: 16,
     mapTypeControl: true,
     mapTypeControlOptions: {
       position: google.maps.ControlPosition.LEFT_BOTTOM
-    }
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.LARGE
+    },
+    draggable: true
   };
 
   // Create Map Object
   var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
+
+  var $canvas = $('#map-canvas');
+  $canvas.bind('touchmove', function(e) {
+    e.preventDefault();
+    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+    map.scrollBy(0, (touch.pageY - touch.pageX));
+    console.log(touch.pageY + ' ' + touch.pageX);
+  });
 
   //Style the Map's color, and labels with styleArray
   var styleArray = [{
@@ -44,82 +56,130 @@ $(function initialize() {
       visibility: "off"
     }]
   }];
+  //Cache Icon Variables
+  var restaurant = "js/icons/restaurant.png";
+  var shopping = "js/icons/mall.png";
+  var outdoors = "js/icons/amphitheater-2.png";
+  var entertainment = "js/icons/movierental.png";
+  var tvLandmark = "js/icons/star-3.png";
 
-  // Implement Style
-  map.setOptions({
-    styles: styleArray
-  });
-
-  //Create Markers
-  var makeMyMarkers = function() {
-    markersArray = [{
+  // Create Model to Hold Knockout Data
+  var viewModel = {
+    myMarkers: ko.observableArray([{
       name: "King Taco",
       location: new google.maps.LatLng(34.146699, -118.147875),
-      icon: "js/icons/restaurant_mexican.png"
+      icon: restaurant
     }, {
-      name: "Penny's Cheesecake Factory from The Big Bang Theory",
+      name: "Penny's Cheesecake Factory",
       location: new google.maps.LatLng(34.145522, -118.150697),
-      icon: "js/icons/tv.png" 
+      icon: tvLandmark
     }, {
       name: "Barney's Beanery",
       location: new google.maps.LatLng(34.146100, -118.148334),
-      icon: "js/icons/bar.png"
+      icon: restaurant
     }, {
       name: "Crepe Studio",
       location: new google.maps.LatLng(34.146976, -118.148322),
-      icon: "js/icons/icecream.png"
+      icon: restaurant
     }, {
       name: "Pop! Champagne Bar",
       location: new google.maps.LatLng(34.147078, -118.149716),
-      icon: "js/icons/winebar.png"
+      icon: restaurant
     }, {
       name: "Pasadena Antique Mall",
       location: new google.maps.LatLng(34.144846, -118.143920),
-      icon: "js/icons/artgallery.png"
+      icon: shopping
     }, {
       name: "Paseo Colorado",
       location: new google.maps.LatLng(34.145246, -118.144314),
-      icon: "js/icons/mall.png"
+      icon: shopping
     }, {
       name: "Colorado Boulevard",
       location: new google.maps.LatLng(34.145700, -118.15000),
-      icon: "js/icons/departmentstore.png"
+      icon: shopping
     }, {
       name: "Pasadena City Hall",
       location: new google.maps.LatLng(34.147633, -118.143566),
-      icon: "js/icons/tv.png"
+      icon: tvLandmark
     }, {
       name: "Memorial Park",
       location: new google.maps.LatLng(34.148336, -118.147454),
-      icon: "js/icons/flowers.png"
+      icon: outdoors
     }, {
       name: "Pasadena Civic Auditorium",
       location: new google.maps.LatLng(34.143771, -118.144274),
-      icon: "js/icons/ticket_office2.png"
+      icon: entertainment
     }, {
       name: "iPic Theaters",
       location: new google.maps.LatLng(34.146569, -118.151480),
-      icon: "js/icons/movierental.png"
-    }];
-    for (var i = 0; i < markersArray.length; i++) {
-        var image = markersArray[i].icon;
-				var marker = new google.maps.Marker({
-					position: markersArray[i].location,
-					map: map,
-					title: markersArray[i].name,
-          icon: image
-				});
+      icon: entertainment
+    }]),
+    search: ko.observable(true),
+    show: ko.observable(false),
+    hide: ko.observable(true),
+    toggleSearch: function() {
+      viewModel.search(!viewModel.search());
+      viewModel.show(!viewModel.show());
+      viewModel.hide(!viewModel.hide());
+    },
+    searchQuery: ko.observable(""),
+    goToLocation: function(marked) {
+      if (viewModel.search()) {
+        viewModel.toggleSearch();
       }
-};
-	makeMyMarkers();
-
-var ViewModel = {
-  myMarkers: ko.observableArray()
-};
-for (var i = 0; i < markersArray; i++) {
-  ViewModel.myMarkers.push(markersArray[i]);
-}
-
-ko.applyBindings(window.ViewModel);
-
+      setTimeout(function() {
+        map.setZoom(16);
+      }, 250);
+      setTimeout(function() {
+        map.panTo(marked.location);
+      }, 500);
+      setTimeout(function() {
+        map.setZoom(19);
+      }, 750);
+    }
+  };
+  var makeMyMarkers = function() {
+    for (var i = 0; i < viewModel.myMarkers().length; i++) {
+      var image = viewModel.myMarkers()[i].icon;
+      var marker = new google.maps.Marker({
+        position: viewModel.myMarkers()[i].location,
+        map: map,
+        title: viewModel.myMarkers()[i].name,
+        icon: image
+      });
+    }
+    map.setOptions({
+      styles: styleArray
+    });
+    ko.applyBindings(viewModel);
+    var oauth = OAuth({
+    consumer: {
+        public: 'gWgPHhf_cUVygsDdtxhxBA',
+        secret: 'LyiUUi2Kl4123DYz5YqXt3jb4sY'
+    },
+    signature_method: 'HMAC-SHA1'
 });
+var yelpURL = 'http://api.yelp.com/v2/search?term=' + viewModel.searchQuery() + '&limit=20&radius_filter=600&ll=34.146580,-118.147700';
+var request_data = {
+    url: yelpURL,
+    method: 'POST',
+    data: {
+        status: 'Hello Ladies + Gentlemen, a signed OAuth request!'
+    }
+};
+var token = {
+    public: 'rE0G5zUpsVWJ8e5UU4adj5H_H7mxZfDz',
+    secret: 'X2AkFXAyJ7gn64rib2FD4cCYQYU'
+};
+
+$.ajax({
+    url: request_data.url,
+    type: request_data.method,
+    data: request_data.data,
+    headers: oauth.toHeader(oauth.authorize(request_data, token))
+}).done(function(data) {
+    console.log(data);
+});
+}();
+});
+
