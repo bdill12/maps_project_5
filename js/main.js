04f3eb1df26d382414c5fc741a7bc5c758ef1118
Brent Dill <bdill12@gmail.com>
2015-05-28 11:01:37 -0700

    Almost done

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(function initialize() {
   // Set Map Options
   var mapOptions = {
     center: oldTownPasadena,
-    zoom: 16,
+    zoom: 17,
     mapTypeControl: true,
     mapTypeControlOptions: {
       position: google.maps.ControlPosition.LEFT_BOTTOM
@@ -21,19 +21,39 @@ $(function initialize() {
   var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
 
-  //Style the Map's color, and labels with styleArray
+  //Style the Map
   var styleArray = [{
-    featureType: "all",
+    featureType: "landscape",
+    elementType: "geometry.fill",
     stylers: [{
-      saturation: -80
+      color: "#7f8c8d"
     }]
   }, {
-    featureType: "road.arterial",
+    featureType: "poi",
+    elementType: "geometry",
+    stylers:[{
+      color: "#2c3e50"
+    }]
+
+  }, {
+    featureType: "poi.park",
     elementType: "geometry",
     stylers: [{
-      hue: "#00ffee"
+      color: "#2ecc71"
+    }]
+  }, {
+    featureType: "poi.medical",
+    elementType: "geometry",
+    stylers: [{
+      color: "#c0392b"
+    }]
+  }, {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{
+      color: "#c0392b"
     }, {
-      saturation: 50
+      saturation: -50
     }]
   }, {
     featureType: "road",
@@ -42,146 +62,147 @@ $(function initialize() {
       visibility: "off"
     }]
   }, {
-    featureType: "poi.business",
+    featureType: "poi",
     elementType: "labels",
     stylers: [{
       visibility: "off"
     }]
   }];
-  //Cache Icon Variables
-  var restaurant = "js/icons/restaurant.png";
-  var shopping = "js/icons/mall.png";
-  var outdoors = "js/icons/amphitheater-2.png";
-  var entertainment = "js/icons/movierental.png";
-  var tvLandmark = "js/icons/star-3.png";
-  //Coffee Shops
-  var coffeeShops = "js/icons/coffee.png";
-  //Mexican Food
-  var icon4bf58dd8d48988d151941735_id = "js/icons/restaurant_mexican.png";
-  //Neighborhoods
-  var icon4f2a25ac4b909258e854f55f_id = "js/icons/citysquare.png";
+  map.setOptions({styles: styleArray});
+
+  var model = {
+    clientId: 'OSDBVMKK4BLXPU14JLBUB0HVLVMNUIRHBTLRA33LUMSFH2BT',
+    clientSecret: 'AE41EWYBOYBGY5AKSW5DBPRQW2DD5MK3Y1YLCLO3KTBQITSG',
+    limit: 25,
+    radius: 600,
+    latitude: 34.146580,
+    longitude: -118.147700
+  };
 
 
   // Create Model to Hold Knockout Data
-  var ViewModel = {
-    search: ko.observable(true),
-    show: ko.observable(false),
-    hide: ko.observable(true),
-    toggleSearch: function() {
-      ViewModel.search(!ViewModel.search());
-      ViewModel.show(!ViewModel.show());
-      ViewModel.hide(!ViewModel.hide());
-    },
-    searchQuery: ko.observable(""),
-    goToLocation: function(marked) {
-      if (ViewModel.search()) {
-        ViewModel.toggleSearch();
+  var ViewModel = function() {
+    var self = this;
+    self.searchQuery = ko.observable("");
+    self.foursquareURL = 'https://api.foursquare.com/v2/venues/explore?client_id='+model.clientId+'&client_secret='+model.clientSecret+'&v=20130815&ll='+model.latitude+','+model.longitude+'&radius='+model.radius+'&limit='+model.limit+'&venuePhotos=1';
+    self.myMarkers = ko.observableArray();
+    self.search = ko.observable(true);
+    self.show = ko.observable(false);
+    self.hide = ko.observable(true);
+    self.viewInfo = ko.observable(false);
+    self.infoTitle = ko.observable();
+    self.infoTwitter = ko.observable();
+    self.infoPhone = ko.observable();
+    self.infoURL = ko.observable();
+    self.infoImage = ko.observable();
+    self.infoAddress1 = ko.observable();
+    self.infoAddress2 = ko.observable();
+    self.infoAddress3 = ko.observable();
+    self.infoTips = ko.observableArray();
+
+    // Assign the Correct Values to each Variable and open Info Window
+    self.infoWindow = function(clicked) {
+      self.infoTitle(clicked.title);
+      self.infoTwitter(clicked.twitter);
+      self.infoPhone(clicked.phone);
+      self.infoURL(clicked.url);
+      self.infoImage(clicked.photoURL);
+      self.infoAddress1(clicked.address1);
+      self.infoAddress2(clicked.address2);
+      self.infoAddress3(clicked.address3);
+      self.infoTips(clicked.tips);
+      self.viewInfo(true);
+    };
+
+    // Close the Info Window
+    self.infoClose = function() {
+      self.viewInfo(false);
+    };
+
+    // Show or Hide the List View
+    self.toggleSearch = function() {
+      self.search(!self.search());
+      self.show(!self.show());
+      self.hide(!self.hide());
+    };
+
+    // Move Map to center on chosen location
+    self.goToLocation= function(marked) {
+      if (self.search()) {
+        self.toggleSearch();
       }
       setTimeout(function() {
-        map.setZoom(16);
-      }, 250);
-      setTimeout(function() {
-        map.panTo(marked.location);
+        map.panTo(marked.position);
       }, 500);
       setTimeout(function() {
         map.setZoom(19);
       }, 750);
-    },
-    myMarkers: ko.observableArray([{
-      name: "King Taco",
-      location: new google.maps.LatLng(34.146699, -118.147875)
-    //  icon: restaurant
-    }, {
-      name: "Penny's Cheesecake Factory",
-      location: new google.maps.LatLng(34.145522, -118.150697)
-    //  icon: tvLandmark
-    }, {
-      name: "Barney's Beanery",
-      location: new google.maps.LatLng(34.146100, -118.148334)
-    //  icon: restaurant
-    }, {
-      name: "Crepe Studio",
-      location: new google.maps.LatLng(34.146976, -118.148322)
-    //  icon: restaurant
-    }, {
-      name: "Pop! Champagne Bar",
-      location: new google.maps.LatLng(34.147078, -118.149716)
-    //  icon: restaurant
-    }, {
-      name: "Pasadena Antique Mall",
-      location: new google.maps.LatLng(34.144846, -118.143920)
-     // icon: shopping
-    }, {
-      name: "Paseo Colorado",
-      location: new google.maps.LatLng(34.145246, -118.144314)
-    //  icon: shopping
-    }, {
-      name: "Colorado Boulevard",
-      location: new google.maps.LatLng(34.145700, -118.15000)
-    //  icon: shopping
-    }, {
-      name: "Pasadena City Hall",
-      location: new google.maps.LatLng(34.147633, -118.143566)
-     // icon: tvLandmark
-    }, {
-      name: "Memorial Park",
-      location: new google.maps.LatLng(34.148336, -118.147454)
-   //   icon: outdoors
-    }, {
-      name: "Pasadena Civic Auditorium",
-      location: new google.maps.LatLng(34.143771, -118.144274)
-    //  icon: entertainment
-    }, {
-      name: "iPic Theaters",
-      location: new google.maps.LatLng(34.146569, -118.151480)
-//      icon: entertainment
-    }])
-  //  parseResults: function(responses) {
-    //  for (var i = 0; i < responses.response.groups[0].items.length; i++) {
-   //     var result = responses.response.groups[0].items[i].venue;
-     //   var venue = {
-       //   name: result.name,
-         // location: new google.maps.LatLng(result.location.lat, result.location.lng),
-//          icon: result.categories[0].name.replace(" ", ""),
-  //        photoURL: result.featuredPhotos.items[0].prefix + result.featuredPhotos.items[0].suffix,
-    //      photoHeight: result.featuredPhotos.items[0].height,
-      //    photowidth: result.featuredPhotos.items[0].width,
-        //  phone: result.contact.formattedPhone,
- //         twitter: result.contact.twitter,
-        //  open: result.hours.status,
-   //     open: result.hours,
-     //     address1: result.location.formattedAddress[0],
-       //   address2: result.location.formattedAddress[1],
-         // address3: result.location.formattedAddress[2],
-        //  price: result.price.message,
- //       price: result.price,
-        //  rating: result.price.rating,
-   //       url: result.url
-     //   };
-    //    if (category == "Coffee Shops") {
-      //    this.venue.icon = 
-  //      }
-   //     ViewModel.myMarkers.push(venue);
-      };
-  //  }
-//  };
-  var makeMyMarkers = function() {
-    for (var i = 0; i < ViewModel.myMarkers().length; i++) {
-      var image = ViewModel.myMarkers()[i].icon;
-      var marker = new google.maps.Marker({
-        position: ViewModel.myMarkers()[i].location,
-        map: map,
-        title: ViewModel.myMarkers()[i].name,
-      //  icon: image
-      });
-    }
+    };
+
+    // Parse results from Foursquare API Venues Database
+    self.parseResults = function(responses) {
+      for (var i = 0; i < responses.response.groups[0].items.length; i++) {
+        var prefix = responses.response.groups[0].items[i];
+        var result = prefix.venue;
+        var myTips = [];
+          for (var t = 0; t < prefix.tips.length; t++) {
+            myTips.push({
+              tipText: prefix.tips[t].text,
+              tipName: prefix.tips[t].user.firstName,
+              tipURL: prefix.tips[t].canonicalUrl,
+              tipPhoto: prefix.tips[t].user.photo.prefix + "44" + prefix.tips[t].user.photo.suffix
+            });
+          }
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(result.location.lat, result.location.lng),
+          map: map,
+          title: result.name,
+          icon: result.categories[0].icon.prefix + "bg_44" + result.categories[0].icon.suffix,
+          phone: result.contact.formattedPhone,
+          twitter: result.contact.twitter,
+          address1: result.location.formattedAddress[0],
+          address2: result.location.formattedAddress[1],
+          address3: result.location.formattedAddress[2],
+          url: result.url,
+          tips: myTips
+        })
+        self.myMarkers.push(marker);
+        google.maps.event.addListener(marker, 'click', function() {
+          self.goToLocation(this);
+          self.infoWindow(this);
+        });
+      }
+    };
+
+  // Search the Foursquare Venues database for results
+  self.foursquareSearch = function() {
+    var fourSearch = self.foursquareURL + "&query='" + self.searchQuery() + "'";
+    $.getJSON(fourSearch, function(response) {
+      self.parseResults(response);
+    });
   };
-  map.setOptions({styles: styleArray});
-  ko.applyBindings(ViewModel);
- // var clientId = 'OSDBVMKK4BLXPU14JLBUB0HVLVMNUIRHBTLRA33LUMSFH2BT';
-//  var clientSecret = 'AE41EWYBOYBGY5AKSW5DBPRQW2DD5MK3Y1YLCLO3KTBQITSG';
- // var foursquareURL = 'https://api.foursquare.com/v2/venues/explore?client_id='+clientId+'&client_secret='+clientSecret+'&v=20130815&ll=34.146580,-118.147700&radius=600&limit=50&venuePhotos=1&query=' + ViewModel.searchQuery();
- // $.getJSON(foursquareURL, function(response){
- //   ViewModel.parseResults(response);
-  //});
+
+  // Close Info Window if user clicks outside it
+  $(document).mouseup(function(e) {
+    var myWindow = $('#infowindow'); 
+    if (!myWindow.is(e.target)
+      && myWindow.has(e.target).length === 0) {
+        self.infoClose();
+    }
+  });
+
+  // Search
+  $('#searchInput').keypress(function(event) {
+    if (event.which == 13) {
+      for (var r = 0; r < self.myMarkers().length; r++) {
+        self.myMarkers()[r].setMap(null)
+      }
+      self.myMarkers([]);
+      self.foursquareSearch();
+    }
+  })
+
+  self.foursquareSearch();
+};
+ko.applyBindings(new ViewModel());
 });
